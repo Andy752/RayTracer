4577fc40040f2712c55639d31986172bece9dee4
Andy752 <1242617185@qq.com>
2020-02-20 10:50:47 +0800

    Ch9 done

--- RayTracer/kernel.cu
+++ RayTracer/kernel.cu
@@ -90,20 +90,22 @@ __global__ void render(vec3* fb, int max_x, int max_y, int ns, camera** cam, hit
 __global__ void create_world(hitable** d_list, hitable** d_world, camera** d_camera) {
 	if (threadIdx.x == 0 && blockIdx.x == 0) {
 		d_list[0] = new sphere(vec3(0, 0, -1), 0.5,
-			new lambertian(vec3(0.8, 0.3, 0.3)));
+			new lambertian(vec3(0.1, 0.2, 0.5)));
 		d_list[1] = new sphere(vec3(0, -100.5, -1), 100,
 			new lambertian(vec3(0.8, 0.8, 0.0)));
 		d_list[2] = new sphere(vec3(1, 0, -1), 0.5,
-			new metal(vec3(0.8, 0.6, 0.2), 1.0));
+			new metal(vec3(0.8, 0.6, 0.2), 0.0));
 		d_list[3] = new sphere(vec3(-1, 0, -1), 0.5,
-			new metal(vec3(0.8, 0.8, 0.8), 0.3));
-		*d_world = new hitable_list(d_list, 4);
+			new dielectric(1.5));
+		d_list[4] = new sphere(vec3(-1, 0, -1), -0.45,
+			new dielectric(1.5));
+		*d_world = new hitable_list(d_list, 5);
 		*d_camera = new camera();
 	}
 }
 
 __global__ void free_world(hitable** d_list, hitable** d_world, camera** d_camera) {
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 5; i++) {
 		delete ((sphere*)d_list[i])->mat_ptr;
 		delete d_list[i];
 	}
@@ -134,7 +136,7 @@ int main() {
 
 	// make our world of hitables & the camera
 	hitable** d_list;
-	checkCudaErrors(cudaMalloc((void**)& d_list, 4 * sizeof(hitable*)));
+	checkCudaErrors(cudaMalloc((void**)& d_list, 5 * sizeof(hitable*)));
 	hitable** d_world;
 	checkCudaErrors(cudaMalloc((void**)& d_world, sizeof(hitable*)));
 	camera** d_camera;
@@ -159,7 +161,7 @@ int main() {
 	std::cerr << "took " << timer_seconds << " seconds.\n";
 
 	// Output FB as Image
-	std::ofstream os("metal.ppm");
+	std::ofstream os("dielectric.ppm");
 	os << "P3\n" << nx << " " << ny << "\n255\n";
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
