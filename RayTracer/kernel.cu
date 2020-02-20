4974770f9ed1533b315cb2b590abd312ee4ff00f
Andy752 <1242617185@qq.com>
2020-02-20 11:01:43 +0800

    Ch11 done

--- RayTracer/kernel.cu
+++ RayTracer/kernel.cu
@@ -76,7 +76,7 @@ __global__ void render(vec3* fb, int max_x, int max_y, int ns, camera** cam, hit
 	for (int s = 0; s < ns; s++) {
 		float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
 		float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
-		ray r = (*cam)->get_ray(u, v);
+		ray r = (*cam)->get_ray(u, v, &local_rand_state);
 		col += color(r, world, &local_rand_state);
 	}
 	rand_state[pixel_index] = local_rand_state;
@@ -100,11 +100,17 @@ __global__ void create_world(hitable** d_list, hitable** d_world, camera** d_cam
 		d_list[4] = new sphere(vec3(-1, 0, -1), -0.45,
 			new dielectric(1.5));
 		*d_world = new hitable_list(d_list, 5);
-		*d_camera = new camera(vec3(-2, 2, 1),
-			vec3(0, 0, -1),
+		vec3 lookfrom(3, 3, 2);
+		vec3 lookat(0, 0, -1);
+		float dist_to_focus = (lookfrom - lookat).length();
+		float aperture = 2.0;
+		*d_camera = new camera(lookfrom,
+			lookat,
 			vec3(0, 1, 0),
 			20.0,
-			float(nx) / float(ny));
+			float(nx) / float(ny),
+			aperture,
+			dist_to_focus);
 	}
 }
 
@@ -165,7 +171,7 @@ int main() {
 	std::cerr << "took " << timer_seconds << " seconds.\n";
 
 	// Output FB as Image
-	std::ofstream os("camera.ppm");
+	std::ofstream os("Defocus Blur.ppm");
 	os << "P3\n" << nx << " " << ny << "\n255\n";
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
